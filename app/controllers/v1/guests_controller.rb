0c670457a51f1dfa9612840e1056e0f165d1384a
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-01-31 22:55:36 +0100

    added photos

--- app/controllers/v1/guests_controller.rb
+++ app/controllers/v1/guests_controller.rb
@@ -1,13 +1,13 @@
 class V1::GuestsController < ApplicationController
   def index
     @guests = Guest.order(created_at: :desc).all
-    render json: @guests
+    render json: @guests.map{|g| guest_as_json(g)}
   end
 
   def create
     @guest = Guest.new(guest_params)
     if @guest.save
-      render json: {success: true, guest: @guest}
+      render json: {success: true, guest: guest_as_json(@guest)}
     else
       render json: {success: false, errors: @guest.errors}
     end
@@ -18,7 +18,13 @@ class V1::GuestsController < ApplicationController
 
   private
 
+  def guest_as_json(guest)
+    guest.as_json.merge(
+      guest.photo.attached? ? {photoUrl: url_for(guest.photo)} : {}
+    )
+  end
+
   def guest_params
-    params.require(:guest).permit(:name, :email)
+    params.require(:guest).permit(:name, :email, :photo)
   end
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,12 @@
 class Guest < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :photo, presence: true
+  has_one_attached :photo
+
+  def as_json_with_photo
+    as_json.merge(
+      photo.attached? ? {photo_url: url_for(photo)} : {}
+    )
+  end
 end
